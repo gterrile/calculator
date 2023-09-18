26e9e3c064692044acfbb3662f4fbe258d1dc4b3
German Terrile <gterrile@gmail.com>
2023-09-18 16:23:53 -0700

    operations completed

--- script.js
+++ script.js
@@ -5,12 +5,121 @@ const total = document.getElementById('totalOperation');
 
 let currentOperation = '0';
 operation.textContent = currentOperation;
+total.textContent = '0';
 
-// let totalOperation = '0';
-// total.textContent = totalOperation;
+
 let operationIsEmpty = true;
 let periodIsUsed = false;
 let operatorIsUsed = false;
+
+function eliminateRightZeros (number) {
+  let numberString = number.toString();
+  let isPeriodPresent = false;
+  // Look for a period in the string
+  for (i = 0; i = numberString.length - 1; i++) {
+    if (numberString[i] == '.') {
+      isPeriodPresent = true;
+      break;
+    }
+  }
+  if (isPeriodPresent) {
+    while (numberString[numberString.length - 1] == '0') {
+      numberString = numberString.slice(0, -1);
+    }
+  }
+  console.log('result:', numberString);
+};
+eliminateRightZeros(900000);
+
+function addOperation(a, b) {
+  let total = a + b;
+  return total.toFixed(8);
+};
+
+function substracOperation(a, b) {
+  let total = a - b;
+  return total.toFixed(10);
+}
+
+function multiplyOperation(a, b) {
+  let total = a * b;
+  return total.toFixed(10);
+}
+
+function divideOperation(a, b) {
+  if (b == 0) {
+    return '∞';
+  }
+  let total = a / b;
+  return total.toFixed(10);
+}
+
+function porcentageOperation(a, b) {
+  let total = (a * b) / 100;
+  return total.toFixed(6);
+}
+
+function generateOperationArguments(str) {
+  let operator = '';
+  let left = '';
+  let right = '';
+  
+  for (i = 0; i < currentOperation.length; i++) {
+    if (currentOperation[i] == '+' || 
+        currentOperation[i] == '-' || 
+        currentOperation[i] == '*' ||
+        currentOperation[i] == '/' || 
+        currentOperation[i] == '%') {
+          operator = currentOperation[i];
+          continue;
+        }
+    if (operator == '') {
+      left += currentOperation[i];  
+    }
+    else {
+      right += currentOperation[i];  
+    }
+  }
+  // Display operation Arguments and Operator
+  console.log('currentOperation', currentOperation);
+  console.log('left:', left);
+  console.log('operator:', operator);
+  console.log('right:', right);
+
+  // Call proper operation function
+
+  if (operator == '+') {
+    let result = addOperation(parseFloat(left), parseFloat(right));
+    total.textContent = result;
+    console.log('result:', result)
+  }
+
+  if (operator == '-') {
+    let result = substracOperation(parseFloat(left), parseFloat(right));
+    total.textContent = result;
+    console.log('result:', result)
+  }
+
+  if (operator == '*') {
+    let result = multiplyOperation(parseFloat(left), parseFloat(right));
+    total.textContent = result;
+    console.log('result:', result)
+  }
+
+  if (operator == '/') {
+    let result = divideOperation(parseFloat(left), parseFloat(right));
+    total.textContent = result;
+    console.log('result:', result)
+  }
+
+  if (operator == '%') {
+    let result = porcentageOperation(parseFloat(left), parseFloat(right));
+    total.textContent = result;
+    console.log('result:', result)
+  }
+
+};
+
 
 // Get values and display them on console
 allKeys.forEach(key => {
@@ -18,6 +127,7 @@ allKeys.forEach(key => {
     
     // Clears the current operation
     if (key.value == 'clear') {
+      total.textContent = '0';
       operation.textContent = '0';
       currentOperation = '0';
       periodIsUsed = false;
@@ -31,21 +141,6 @@ allKeys.forEach(key => {
         operatorIsUsed = true;
         periodIsUsed = false;
     }
-
-    // Validates not allowing two operators periods together
-    // if (key.classList.contains('period') && currentOperation != '0') {
-    //   let lastCharacterIndex = currentOperation.length - 1;
-    //   if (currentOperation[lastCharacterIndex] != '.') {
-    //     currentOperation += key.value;
-    //     operation.textContent = currentOperation;
-    //   }
-    // }
-
-    // // Builds up the operation on a string
-    // if (currentOperation != '0' && key.value != 'clear' && key.value != '=') {
-    //   currentOperation += key.value;
-    //   operation.textContent = currentOperation;
-    // }
 
     // Builds up the operation on a string
     if (currentOperation != '0' && key.classList.contains('number')) {
@@ -70,33 +165,8 @@ allKeys.forEach(key => {
    
     // Builds the left, right and operator parts of the equation
     if (currentOperation != '0' && key.value == '=') {
-      let operator = '';
-      let left = '';
-      let right = '';
-      
-      for (i = 0; i < currentOperation.length; i++) {
-        if (currentOperation[i] == '+' || 
-            currentOperation[i] == '-' || 
-            currentOperation[i] == '*' ||
-            currentOperation[i] == '/' || 
-            currentOperation[i] == '%') {
-              operator = currentOperation[i];
-              continue;
-            }
-        if (operator == '') {
-          left += currentOperation[i];  
-        }
-        else {
-          right += currentOperation[i];  
-        }
-      }
-      console.log('currentOperation', currentOperation);
-      console.log('left:', left);
-      console.log('operator:', operator);
-      console.log('right:', right);
+      generateOperationArguments(currentOperation);
     }
-
-
 
   })
 });
